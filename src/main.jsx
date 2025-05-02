7abdcd12c79ee4db4c25d971a1ead5a2ad6dcbe6
Gustavo Orelio <oreliogustavo@gmail.com>
2025-05-01 22:25:54 -0300

    chore: add react query setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,28 @@
 import './index.css'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
-import HomePage from './pages/home.jsx'
-import LoginPage from './pages/login.jsx'
-import NotFoundPage from './pages/not-found.jsx'
-import SignupPage from './pages/signup.jsx'
+import HomePage from './pages/home'
+import LoginPage from './pages/login'
+import NotFoundPage from './pages/not-found'
+import SignupPage from './pages/signup'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 )
